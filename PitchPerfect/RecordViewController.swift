45afd80ee888bc72213882a9546a532b2102232a
Gareth Hunt <ghunt03@gmail.com>
2015-12-06 16:02:03 +1100

    Updated Readme.md

--- PitchPerfect/RecordViewController.swift
+++ PitchPerfect/RecordViewController.swift
@@ -10,18 +10,21 @@ import UIKit
 import AVFoundation
 class RecordViewController: UIViewController, AVAudioRecorderDelegate {
 
+    //initialising button and label outlets
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var recordLabel: UILabel!
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var resumeButton: UIButton!
     @IBOutlet weak var pauseButton: UIButton!
     
+    //variables for the handling of the audio recording
     var audioRecorder:AVAudioRecorder!
     var recordedAudio:RecordedAudio!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
     override func viewWillAppear(animated: Bool) {
         stopButton.hidden = true
         recordButton.enabled = true
@@ -33,21 +36,25 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
     func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
         if (flag) {
+            //if recording was finished successfully save details in RecordedAudio object and call segue to next screen
             recordedAudio = RecordedAudio(filePath: recorder.url, title:recorder.url.lastPathComponent!)
             self.performSegueWithIdentifier("stopRecordingSegue", sender: recordedAudio)
         }
         else {
+            //if the recording did not finish successfully alert user
             let alertController = UIAlertController(title: "Pitch Perfect", message:
                 "There was a problem with the recording", preferredStyle: UIAlertControllerStyle.Alert)
             alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default,handler: nil))
-            
             self.presentViewController(alertController, animated: true, completion: nil)
         }
     }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if (segue.identifier == "stopRecordingSegue") {
+            //move to next screen and pass RecordedAudio object
             let playSoundsVC:PlaySoundsViewController = segue.destinationViewController as! PlaySoundsViewController
             let data = sender as! RecordedAudio
             playSoundsVC.receivedAudio = data
@@ -55,6 +62,7 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
     }
 
     @IBAction func stopRecording(sender: AnyObject) {
+        //stops recording
         stopButton.hidden = true
         recordLabel.text = "Tap to record"
         recordButton.enabled = true
@@ -72,6 +80,7 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
     }
     
     func pauseResumeRecording() {
+        //function to allow the user to pause / resume the recording
         if (audioRecorder.recording) {
             audioRecorder.pause()
             recordLabel.text = "Recording paused"
@@ -86,28 +95,29 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
         }
     }
     
-
     @IBAction func recordAudio(sender: AnyObject) {
+        //update label and show pause / stop button
         recordLabel.text = "Recording in progress"
         stopButton.hidden = false
         recordButton.enabled = false
         pauseButton.hidden = false
         
+        //sets the file path for the recorded audio
         let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
         let recordingName = "my_audio.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = NSURL.fileURLWithPathComponents(pathArray)
         print(filePath)
         
+        //initiates recording session
         let session = AVAudioSession.sharedInstance()
         try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        //starts recording
         try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
         audioRecorder.delegate = self
         audioRecorder.meteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
     }
-    
-    
 }
-
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# IOSND01---Pitch-Perfect
+# IOSND01 Pitch Perfect App
+## Overview
+This project was built as part of the Udacity iOS Nanodegree and was my first attempt at writing an iOS app. This specifications for this project was to develop a iPhone app, which can record audio and playback the audio after applying different filters. The sound effects include:
+
+- Slow
+- Fast
+- Chipmunk
+- Darth Vader
+- Reverb
+- Echo
+
+This app also needed to provide the end user with the ability to pause and resume the recording. 
+
+The audio features are built using the AVAudioFoundation class.
+
+##Instructions
+###Running the App
+The app can be downloaded through this repository. Once downloaded you can open the project by double clicking on the PitchPerfect.xcodeproj file. After the project has opened in xCode you can run the app through the simulator.
+
+**Note: **this project has been tested on iPhone 5S, using xCode 7.1.1 and Swift 2.0
+
+###Using the App
+1. Click on the record button to start recording
+2. To pause recording click on the pause button
+3. To resume recording tap the start button
+4. To stop the recording tap the stop button
+5. Once the stop button is pressed you will be taken to the playback screen. 
+6. You will then be presented with a number of different playback options.
+7. Changing sound effects will restart the recorded audio
+
+
+
+
